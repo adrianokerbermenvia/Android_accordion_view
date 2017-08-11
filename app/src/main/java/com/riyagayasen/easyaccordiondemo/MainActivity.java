680b6680d40761a3d00598f0e0a05d555a33c6f2
Adriano Kerber <adriano.kerber@menvia.com>
2017-08-11 16:06:49 -0300

    Another test

--- app/src/main/java/com/riyagayasen/easyaccordiondemo/MainActivity.java
+++ app/src/main/java/com/riyagayasen/easyaccordiondemo/MainActivity.java
@@ -13,7 +13,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AccordionView accordionView = (AccordionView) findViewById(R.id.test_accordion);
+        AccordionView accordionView2 = (AccordionView) findViewById(R.id.test_accordion2);
         TextView textView = (TextView) accordionView.findViewById(R.id.textView);
         textView.setText("This is a demo accordion with text added dynamically This is a demo accordion with text added dynamically This is a demo accordion with text added dynamically This is a demo accordion with text added dynamically This is a demo accordion with text added dynamically This is a demo accordion with text added dynamically This is a demo accordion with text added dynamically");
+
+        //////////////////////////////
+        // A1
+
+        //accordionView.setBackground(getResources().getDrawable(R.drawable.background_magenta));
+        //accordionView.setBackgroundColor(getResources().getColor(R.color.yellow));
+        accordionView.setHeadingBackGround(R.drawable.background_yellow);
+        //accordionView.setBodyBackGround(getResources().getDrawable(R.drawable.background_yellow));
+
+
+        //////////////////////////////
+        // A2
+
+        accordionView2.setBackground(getResources().getDrawable(R.drawable.background_magenta));
+        accordionView2.setHeadingBackGround(R.drawable.background_yellow);
+        //accordionView2.setHeadingBackGroundColor(R.color.test);
     }
 }
